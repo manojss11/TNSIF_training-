6e600117e4ef8200eaf815ab54e8599bc4142e09
Manoj SS <manojssmanu777@gmail.com>
2026-08-05 11:04:26 +0530

    Frist commit

---
+++ src/org/tnsif/acc/c2tc/scannerclass_bufferreader/AdvancedScannerMethod.java
@@ -0,0 +1,10 @@
+package org.tnsif.acc.c2tc.scannerclass_bufferreader;
+
+public class AdvancedScannerMethod {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
