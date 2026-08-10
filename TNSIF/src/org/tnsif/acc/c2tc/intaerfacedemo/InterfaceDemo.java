8ff21e6c367315ffd95ec124267332a70c854b8e
Manoj SS <manojssmanu777@gmail.com>
2026-08-10 10:54:20 +0530

    commit

---
+++ TNSIF/src/org/tnsif/acc/c2tc/intaerfacedemo/InterfaceDemo.java
@@ -0,0 +1,37 @@
+package org.tnsif.acc.c2tc.intaerfacedemo;
+
+class SmartLight implements SmartDevice{
+
+	@Override
+	public void turnOn() {
+		System.out.println("SmartLight is On");
+		
+	}
+
+	@Override
+	public void turnOff() {
+		System.out.println("SmartLight is Off");
+		
+	}
+
+	@Override
+	public void standbymode() {
+		System.out.println("SmartLight is Standbymode");
+		
+	}
+	
+}
+
+public class InterfaceDemo {
+
+	public static void main(String[] args) {
+		SmartDevice smart=new SmartLight();
+		smart.turnOn();
+	    smart.turnOff();
+	    smart.standbymode();
+		
+		
+
+	}
+
+}
---
+++ TNSIF/src/org/tnsif/acc/c2tc/intaerfacedemo/SmartDevice.java
@@ -0,0 +1,7 @@
+package org.tnsif.acc.c2tc.intaerfacedemo;
+
+public interface SmartDevice {
+	void turnOn();
+	void turnOff();
+	void standbymode();
+}
